e1a2e59f6206d6794e207d14567ee605f7db4eaa
Mads Harder <mads987a@gmail.com>
2018-08-19 14:43:49 +0200

    Made a config files folder and cleaned up the code

--- config-files/.bashrc
+++ config-files/.bashrc
@@ -19,6 +19,13 @@ shopt -s histappend
 HISTSIZE=1000
 HISTFILESIZE=2000
 
+# test if .scripts is in PATH, if it is not then it adds it
+SCRIPT_DIR="$HOME/.scripts"
+if [[ ! $PATH = *$SCRIPT_DIR* ]]; then
+  export PATH=$PATH:$HOME/.scripts
+fi
+
+
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
@@ -62,8 +69,14 @@ fi
 #    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 #fi
 
+# Get witch git branch you're on
+# TODO add color to git branch
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/-[\1]/'
+}
+
 if [ "$color_prompt" = yes ]; then
-    PS1='┌──[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]]-[\[\033[01;34m\]\w\[\033[00m\]]\n└──╼ \$ '
+    PS1='┌──[${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]]$(parse_git_branch)-[\[\033[01;34m\]\w\[\033[00m\]]\n└──╼ \$ '
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
--- install.sh
+++ install.sh
@@ -7,20 +7,43 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 BACKUPS_DIR="$DOTFILES_DIR/backup/"
 mkdir -p $BACKUPS_DIR
 
+CONFIG_FILES_DIR="$DOTFILES_DIR/config-files"
+
+# List of dotfiles
 DOTFILES_LIST=(
   ".bashrc"
+  ".vimrc"
 )
 
-# Backing up every files in the DOTFILES_LIST
-echo "started backing up the files before linking them"
+# Colors name
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_RESET="\033[0m"
+
+# Warning message
+WARNING_MESSAGE="${COLOR_RED}WARNING:${COLOR_RESET}"
+
+
+# Backing up files
+echo -e "${COLOR_GREEN}backing up the config files${COLOR_RESET}"
 for i in "${DOTFILES_LIST[@]}"; do
   echo "backing up $i"
   cp -f $HOME/$i $BACKUPS_DIR
 done
 
 # Making the symbolic link
-echo -e "\nstarted linking files"
+echo -e "${COLOR_GREEN}making symbolic links${COLOR_RESET}"
 for i in "${DOTFILES_LIST[@]}"; do
   echo -n "made a symbolic link for $i "
-  ln -sfv $DOTFILES_DIR/$i ~
+  ln -sfv $CONFIG_FILES_DIR/$i ~
 done
+
+# Downloading the scripts repo
+SCRIPTS_DIR="$HOME/.scripts"
+if [ -d "$SCRIPTS_DIR" ]; then
+  echo -e "$WARNING_MESSAGE $HOME/.scripts does already exist"
+else
+  echo -e "${COLOR_GREEN}cloning scripts${COLOR_RESET}"
+  git clone https://github.com/mads10m/.scripts.git ~/.scripts
+fi
